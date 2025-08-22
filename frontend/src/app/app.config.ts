e42942e4b391934016c5267a08ea7e3cbe8cf4da
Cesar Nilton Vincenty Funes <cnvincentyf@soe.uagrm.edu.bo>
2025-08-22 14:49:42 -0400

    Logro: Frontend Enlaces 5

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -11,6 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
     importProvidersFrom(ReactiveFormsModule),
-    provideRouter(routes)
+    provideRouter(routes, withHashLocation())
   ]
 };
