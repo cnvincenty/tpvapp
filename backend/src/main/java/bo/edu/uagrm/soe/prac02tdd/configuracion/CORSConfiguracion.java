d72d3be3f3aa3d061a10fb07d709b771966b5d17
Cesar Nilton Vincenty Funes <cnvincentyf@soe.uagrm.edu.bo>
2025-08-24 21:22:20 -0400

    Logro: Puerto 9797 habilitado

--- backend/src/main/java/bo/edu/uagrm/soe/prac02tdd/configuracion/CORSConfiguracion.java
+++ backend/src/main/java/bo/edu/uagrm/soe/prac02tdd/configuracion/CORSConfiguracion.java
@@ -10,7 +10,7 @@ public class CORSConfiguracion implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost", "http://localhost:9595", "http://localhost:4200")
+                .allowedOrigins("http://localhost", "http://localhost:9797", "http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9595/api',
+  apiUrl: 'http://localhost:9797/api',
 };
